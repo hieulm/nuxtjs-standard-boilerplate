945da47ca8477f9d52b09058d4a63c8802e836c5
Hieu Le <hieulm@live.com>
2020-12-16 08:10:34 +0700

    first commit

---
+++ .nuxt/router.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL } from '@nuxt/ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _63ba7930 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
+const _5b0e4690 = () => interopDefault(import('../pages/todos.vue' /* webpackChunkName: "pages/todos" */))
+const _f91b5816 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/about",
+    component: _63ba7930,
+    name: "about"
+  }, {
+    path: "/todos",
+    component: _5b0e4690,
+    name: "todos"
+  }, {
+    path: "/",
+    component: _f91b5816,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+function decodeObj(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') {
+      obj[key] = decodeURIComponent(obj[key])
+    }
+  }
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    const r = resolve(to, current, append)
+    if (r && r.resolved && r.resolved.query) {
+      decodeObj(r.resolved.query)
+    }
+    return r
+  }
+
+  return router
+}
